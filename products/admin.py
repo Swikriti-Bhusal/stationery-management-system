5ddd5bd898f36b49e1790301c527a1a9a90ee43a
Swikriti-Bhusal <swikritibhusal07@gmail.com>
2026-05-09 09:01:31 +0545

    Added Product app with models and migrations

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'is_available']
+    list_filter = ['category', 'is_available']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
