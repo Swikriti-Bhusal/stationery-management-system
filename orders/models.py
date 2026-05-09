e1296a428dbf227b19b7255a821bfbce17e1c786
Swikriti-Bhusal <swikritibhusal07@gmail.com>
2026-05-09 18:35:45 +0545

    Updated orders model and added initial migration

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
+    shipping_address = models.TextField()
+    phone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"Order #{self.id} - {self.user.full_name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price_at_time = models.DecimalField(max_digits=10, decimal_places=2)  # Price when ordered
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
