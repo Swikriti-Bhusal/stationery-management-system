65e910b08639e524520fecb6c76e1e7742ce3bb5
Swikriti-Bhusal <swikritibhusal07@gmail.com>
2026-05-12 16:23:46 +0545

    Updated products and homepage, added category image support

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.utils.text import slugify
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=120, unique=True, blank=True)
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)  
 
     def save(self, *args, **kwargs):
         if not self.slug:
