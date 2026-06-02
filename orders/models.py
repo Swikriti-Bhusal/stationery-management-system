204127fcea76665a0d7ee53af992767643a1ad9c
Swikriti-Bhusal <swikritibhusal07@gmail.com>
2026-06-02 14:15:28 +0545

    Updated project files and templates

--- orders/models.py
+++ orders/models.py
@@ -61,39 +61,3 @@ class Payment(models.Model):
     def __str__(self):
         return f"Payment for Order #{self.order.id} - {self.payment_status}"
 
-
-# from django.db import models
-# from django.contrib.auth import get_user_model
-# from products.models import Product
-
-# User = get_user_model()
-
-# class Order(models.Model):
-#     STATUS_CHOICES = (
-#         ('pending', 'Pending'),
-#         ('delivered', 'Delivered'),
-#         ('cancelled', 'Cancelled'),
-#     )
-
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-#     order_date = models.DateTimeField(auto_now_add=True)
-#     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-#     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    
-#     shipping_address = models.TextField()
-#     phone = models.CharField(max_length=15)
-    
-#     def __str__(self):
-#         return f"Order #{self.id} - {self.user.full_name}"
-
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.PositiveIntegerField()
-#     price_at_time = models.DecimalField(max_digits=10, decimal_places=2)  # Price when ordered
-
-#     def __str__(self):
-#         return f"{self.quantity} x {self.product.name}"
-    
-#     def get_total_price(self):
-#         return self.quantity * self.price_at_time
--- orders/urls.py
+++ orders/urls.py
@@ -25,27 +25,3 @@ urlpatterns = [
 
 
 
-# from django.urls import path
-# from . import views
-
-# app_name = 'orders'  # This is required for namespace to work
-
-# urlpatterns = [
-#     # Customer order URLs
-#      path('checkout/', views.checkout, name='checkout'),
-#      path('place-order/', views.place_order, name='place_order'),
-#     path('my-orders/', views.my_orders, name='my_orders'),
-#     path('success/<int:order_id>/', views.order_success, name='order_success'),
-#     path('detail/<int:order_id>/', views.order_detail, name='order_detail'),
-    
-#     # Admin order URLs
-
-#         # Admin order URLs
-# path('orders/', views.admin_order_list, name='admin_order_list'),
-# path('orders/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-# path('orders/<int:order_id>/update-status/', views.update_order_status, name='update_order_status'),
-#     # path('admin/orders/', views.admin_order_list, name='admin_order_list'),  # This name will be accessed as orders:admin_order_list
-#     # path('admin/orders/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-#     # path('admin/orders/<int:order_id>/update-status/', views.admin_update_order_status, name='update_order_status'),
-
-# ] 
